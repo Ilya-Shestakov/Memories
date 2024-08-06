2a16fc7827747e269eeab2cb168d10ef2e062074
ilya <ilya.shestakov08@gmail.com>
2024-08-06 14:22:30 +0300

    Design activity login and registration

---
+++ app/src/main/java/com/example/memories/StartActivity/Login.kt
@@ -0,0 +1,12 @@
+package com.example.memories.StartActivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.memories.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
